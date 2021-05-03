bcc40b5611e3e3289bf71ca925f067aaab1016c2
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-05-03 17:20:04 -0500

    nomination cards

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const OMDB_API_KEY = "b415e062";
 
+$("#search-input").on("search", function (e) {
+	const $searchBar = $(this);
+	const input = $searchBar.val();
+	$searchBar.val("");
+
+    //TODO: replace API KEY if it works
+	const queryURL = "https://www.omdbapi.com/?s=" + input + "&apikey=trilogy&p=25";
+	$.ajax({
+		url: queryURL,
+		method: "GET",
+	}).then((res) => {
+
+	});
+});
