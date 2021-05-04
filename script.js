28075fcd48f8bd533300acf49ccb395e5ef08e4a
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-05-03 21:39:20 -0500

    show search result, click to show id

--- script.js
+++ script.js
@@ -14,15 +14,42 @@ $("#search-input").on("search", function (e) {
 		url: queryURL,
 		method: "GET",
 	}).then(res => {
-        if(res.Response === "true") {
+        if(res.Response === "True") {
+            $results.empty();
+
             for(const movie of res.Search) {
                 const poster = movie.Poster;
                 const title = movie.Title;
                 const year = movie.Year;
                 const id = movie.imdbID;
+
+                const $movieCard = $(`
+                    <div class="card bg-dark text-white movie-card">
+						<img
+							src="${poster}"
+							alt="${title}"
+							class="card-img"
+						/>
+						<div class="card-img-overlay">
+							<div class="card-body">
+								<div class="card-title">
+									${title} (${year})
+								</div>
+							</div>
+						</div>
+
+						<button class="nominate-btn" data-id="${id}" onclick="nominate(this)">Nominate</button>
+					</div>
+                `);
+
+                $results.append($movieCard);
             }
         } else {
             $results.text(res.Error);
         }
 	});
 });
+
+function nominate(btn) {
+    console.log($(btn).data("id"));
+}
