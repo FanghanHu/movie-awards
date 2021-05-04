ad9fcb9d74a2e32b9cba1d7dc758e817ba65f3f4
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-05-03 22:43:19 -0500

    pagination

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const OMDB_API_KEY = "b415e062";
 
 const $nomineeList = $("#nominee-list");
 const $results = $("#results");
+const $nav = $("#nav");
 
 $("#search-input").on("search", function (e) {
 	const $searchBar = $(this);
@@ -17,15 +18,16 @@ function search(input, currentPage = 1) {
 		method: "GET",
 	}).then(res => {
         if(res.Response === "True") {
-            //empty search section
-            $results.empty();
-
             //updage pagination
             const totalPages = Math.ceil(res.totalResults / 10);
+            $nav.empty();
+            $nav.append(createPagination(input, totalPages, currentPage));
 
+            //empty search section
+            $results.empty();
             //add moviews
             for(const movie of res.Search) {
-                const poster = movie.Poster;
+                const poster = movie.Poster.startsWith("h")?movie.Poster:"http://placehold.it/200x300";
                 const title = movie.Title;
                 const year = movie.Year;
                 const id = movie.imdbID;
@@ -63,5 +65,45 @@ function nominate(btn) {
 }
 
 function createPagination(input, totalPages, currentPage=1) {
-    
+    const createPaginationButton = (text, targetPage) => {
+        return $(`
+            <li class="page-item${targetPage===currentPage?" active":""}${targetPage?"":" disabled"}"><button class="page-link" ${targetPage?`onclick="search('${input}', ${targetPage})"`:``}>${text}</button></li>
+        `)
+    }
+    const $pagination = $(`
+        <ul class="pagination">
+        </ul>
+    `);
+
+    if(currentPage > 1) {
+        $pagination.append(createPaginationButton("Previous", currentPage-1));
+    }
+
+    if(currentPage > 3) {
+        $pagination.append(createPaginationButton(1, 1));
+        $pagination.append(createPaginationButton("..."));
+    }
+
+    for(let i = 2; i > 0; i--) {
+        if(currentPage - i > 0) {
+            $pagination.append(createPaginationButton(currentPage - i, currentPage - i));
+        }
+    }
+
+    $pagination.append(createPaginationButton(currentPage, currentPage));
+
+    for(let i = 1; i <= 2 && currentPage + i <= totalPages; i++) {
+        $pagination.append(createPaginationButton(currentPage + i, currentPage + i));
+    }
+
+    if(totalPages - currentPage > 2) {
+        $pagination.append(createPaginationButton("..."));
+        $pagination.append(createPaginationButton(totalPages, totalPages));
+    }
+
+    if(currentPage < totalPages) {
+        $pagination.append(createPaginationButton("Next", currentPage+1));
+    }
+
+    return $pagination;
 }
