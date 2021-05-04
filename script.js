5d0e9bb4cfda89ef2202e57aa4b838c3d9c2543e
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-05-03 21:24:30 -0500

    show error

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 const OMDB_API_KEY = "b415e062";
 
+const $nomineeList = $("#nominee-list");
+const $results = $("#results");
+
 $("#search-input").on("search", function (e) {
 	const $searchBar = $(this);
 	const input = $searchBar.val();
 	$searchBar.val("");
 
-    //TODO: replace API KEY if it works
-	const queryURL = "https://www.omdbapi.com/?s=" + input + "&apikey=trilogy&p=25";
+    //search for user input
+	const queryURL = "https://www.omdbapi.com/?s=" + input + "&apikey=" + OMDB_API_KEY;
 	$.ajax({
 		url: queryURL,
 		method: "GET",
-	}).then((res) => {
-
+	}).then(res => {
+        if(res.Response === "true") {
+            for(const movie of res.Search) {
+                const poster = movie.Poster;
+                const title = movie.Title;
+                const year = movie.Year;
+                const id = movie.imdbID;
+            }
+        } else {
+            $results.text(res.Error);
+        }
 	});
 });
